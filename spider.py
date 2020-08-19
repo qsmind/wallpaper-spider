6507e2fb93266e17df14915954cecde8c485ff7d
qsmind <2321589901@qq.com>
2020-08-19 23:34:32 +0800

    get_html finished

--- spider.py
+++ spider.py
@@ -7,7 +7,6 @@ from queue import Queue
 
 import rdhd
 
-
 # 从首页获取套图链接
 # 访问套图链接，获取每张图片链接
 # 创建套图目录
@@ -20,34 +19,42 @@ def create_dir(name):
 
 #生产者只请求页面
 class producer(threading.Thread):
-    def __init__(self,i,page_queue):
+    def __init__(self,i):
         super().__init__()
         self.i = i
-        self.page_queue = page_queue
+        # self.page_queue = page_queue
     
     #复写run方法
     def run(self):
         #任务队列不为空就取数据执行
         while True:
-            if self.page_queue.empty():
+            if page_queue.empty():
                 break
             try:
-                q = self.page_queue.get(block=False)
+                # time.sleep(5)
+                q = page_queue.get()
                 print(self.i, '开始任务%s======='%(q))
                 # url = 'https://careers.tencent.com/tencentcareer/api/post/Query?keyword=python&pageIndex=%s&pageSize=10'%(q)
                 url = 'http://spotlight.shijuewuyu.com/?page=%s'%(q)
-                html=self.get_html(url)# 获取主页信息
-                pic_list = []  # 二维列表，格式：[链接，套图名称,更新时间]
-                get_pic_list(html, pic_list)  # 获取套图      
-                
-                for link in pic_list:  # 爬取单个页面套图
-                    j += 1  # 已获取的套图数量
-                    html = get_html(link[0])  # 获取网页
-                    src=[]
-                    length = get_pic_link(html,src)  # 获取图片链接
-                    # 将图片链接存入队列
-                    for ref in src:
-                        response_q.put(ref)    
+                print(url)
+                try:
+                    html=self.get_html(url)# 获取主页信息
+                    pic_list = []  # 二维列表，格式：[链接，套图名称,更新时间]
+                    self.get_pic_list(html, pic_list)  # 获取套图      
+                    # print(pic_list)
+                    for link in pic_list:  # 爬取单个页面套图
+                        # j += 1  # 已获取的套图数量
+                        print(type(link))
+                        print(link)
+                        html = self.get_html(link)  # 获取网页
+                        src=[]
+                        self.get_pic_link(html,src)  # 获取图片链接
+                        # 将图片链接存入队列
+                        for ref in src:
+                            response_q.put(ref)  
+                    time.sleep(1) 
+                except:
+                    pass 
             except:
                 pass
             
@@ -75,24 +82,28 @@ class producer(threading.Thread):
     # 获取套图
     def get_pic_list(self,html, pic_list):
         try:
-            temp = []
+            # temp = []
             soup = BeautifulSoup(html, "html.parser")
             # print(soup)
-            pins = soup.find_all('div','container')
-            # print(pins[1])
-            for li in pins[1].find_all('li'):
-                # temp.append('https://555zui.com/'+li.find('a').get('href'))
-                temp.append('https://www.2456cc.com/'+li.find('a').get('href'))
-                temp.append(li.find('a').get('title'))
-                temp.append(li.find('div','subtitle text-time text-overflow').string)
-                pic_list.append(temp)
-                temp = []
+            wrapper = soup.find('div','wrapper')
+            for href in wrapper.find_all('a'):
+                pic_list.append(href.attrs["href"])
+            # pic_list.append(wrapper.find('a').get('href'))
+            # href = soup.find_all('a','href')
+            # # print(pins[1])
+            # for link in href:
+            #     # temp.append('https://555zui.com/'+li.find('a').get('href'))
+            #     # temp.append('https://www.2456cc.com/'+li.find('a').get('href'))
+            #     # temp.append(li.find('a').get('title'))
+            #     # temp.append(li.find('div','subtitle text-time text-overflow').string)
+            #     pic_list.append(link.find('a').get('href'))
+                # temp = []
                 # print(pic_list)
         except:
             # print('获取套图失败！')
             return ''
 
-    # 获取每套图片链接
+    # 获取图片链接
     def get_pic_link(self,html,src):
         soup = BeautifulSoup(html, "html.parser")
         # 获取初始图片链接
@@ -157,14 +168,6 @@ class consumer(threading.Thread):
             print('创建目录失败！')
             return 
 
-        # headers = {
-        #     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240",
-        #     'Connection': 'Keep-Alive',
-        #     'Referer': "http://www.mzitu.com/99566"
-        # }
-
-        # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
-
         i=1
         for url in src:
             err=0
@@ -196,12 +199,16 @@ class consumer(threading.Thread):
  
  
 lock = threading.Lock()  #锁
+page_queue = Queue()
 response_q = Queue()     #消费者队列
 flag = False             #表示生产者线程是否都结束
 
 if __name__ == '__main__':
-    #创建生产者任务队列
-    page_queue = Queue()
+    # 创建套图目录
+    create_dir("pic")
+    
+    # #创建生产者任务队列
+    # page_queue = Queue()
     for i in range(1,175):
         page_queue.put(i)
  
@@ -210,26 +217,30 @@ if __name__ == '__main__':
     p_tread = []
     cousumer_name = ['c1','c2','c3']
     c_tread = []
+    
+    p_crawl = producer(i)
+    p_crawl.start()
+    p_crawl.join()
  
-    #启动三个生产者线程
-    for i in producer_name:
-        p_crawl = producer(i,page_queue)
-        p_crawl.start()
-        p_tread.append(p_crawl)
+    # #启动三个生产者线程
+    # for i in producer_name:
+    #     p_crawl = producer(i,page_queue)
+    #     p_crawl.start()
+    #     p_tread.append(p_crawl)
  
-    #启动三个消费者线程
-    for j in cousumer_name:
-        c_crawl = consumer(j)
-        c_crawl.start()
-        c_tread.append(c_crawl)
+    # #启动三个消费者线程
+    # for j in cousumer_name:
+    #     c_crawl = consumer(j)
+    #     c_crawl.start()
+    #     c_tread.append(c_crawl)
  
  
-    #生产者阻塞主线程
-    for threadi in p_tread:
-        threadi.join()
-    #生产者都死了把flag变成True，可以让消费者做判断
-    flag = True
+    # #生产者阻塞主线程
+    # for threadi in p_tread:
+    #     threadi.join()
+    # #生产者都死了把flag变成True，可以让消费者做判断
+    # flag = True
  
-    #消费者阻塞主线程
-    for threadi in c_tread:
-        threadi.join()
+    # #消费者阻塞主线程
+    # for threadi in c_tread:
+    #     threadi.join()
